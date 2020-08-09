d2dbba909ed7d98e2ca36b4f1d105bf577b33fbe
chea-young <study.lchy@gmail.com>
2020-08-09 12:57:36 +0900

    Ch.02 EditView & Button

--- app/src/main/java/com/cheayoung/androidkotlin/MainActivity.kt
+++ app/src/main/java/com/cheayoung/androidkotlin/MainActivity.kt
@@ -9,5 +9,10 @@ class MainActivity : AppCompatActivity() { // 앱이 최초 실행했을 때 수
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // xml 화면 뷰를 연결
         tv_title.setText("Hello...") //text의 값을 변경
+        button_gettext.setOnClickListener {
+            //텍스트에 입력되어있는 값을 가지고 와서 텍스트 뷰에 뿌려준다.
+            var resultText = et_id.text.toString() //문자열 형태로 변화해서 가져오는 것
+            tv_result.setText(resultText)
+        }
     }
 }
