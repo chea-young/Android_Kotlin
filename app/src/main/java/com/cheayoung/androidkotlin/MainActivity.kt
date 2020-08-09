81316784915fcca2874c8e9391839f326c5d329d
chea-young <study.lchy@gmail.com>
2020-08-09 12:43:58 +0900

    Ch.01 TestView

--- app/src/main/java/com/cheayoung/androidkotlin/MainActivity.kt
+++ app/src/main/java/com/cheayoung/androidkotlin/MainActivity.kt
@@ -2,10 +2,12 @@ package com.cheayoung.androidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { // 앱이 최초 실행했을 때 수행.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // xml 화면 뷰를 연결
+        tv_title.setText("Hello...") //text의 값을 변경
     }
 }
