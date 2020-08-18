029f74fff94f41374e75c523ddd43ec3662a7d9f
chea-young <study.lchy@gmail.com>
2020-08-18 20:04:20 +0900

    https://qlyh8.tistory.com/66 보면서 NavigationView 화면 이동하는거 해보기

--- app/src/main/java/com/cheayoung/androidkotlin/MainActivity.kt
+++ app/src/main/java/com/cheayoung/androidkotlin/MainActivity.kt
@@ -3,13 +3,27 @@ package com.cheayoung.androidkotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.btn_navi
+import kotlinx.android.synthetic.main.activity_main.layout_drawer
+import kotlinx.android.synthetic.main.activity_main.naviView
+import kotlinx.android.synthetic.main.activity_navigation.*
 
-class MainActivity : AppCompatActivity() { // 앱이 최초 실행했을 때 수행.
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener { // 앱이 최초 실행했을 때 수행.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // xml 화면 뷰를 연결
         tv_title.setText("Hello...") //text의 값을 변경
+
+        btn_navi.setOnClickListener{
+            layout_drawer.openDrawer(GravityCompat.START)// START : left, END : right 랑 같은 말
+        }
+        naviView.setNavigationItemSelectedListener (this)
+
         button_gettext.setOnClickListener {
             //텍스트에 입력되어있는 값을 가지고 와서 텍스트 뷰에 뿌려준다.
             var resultText = et_id.text.toString() //문자열 형태로 변화해서 가져오는 것
@@ -44,5 +58,26 @@ class MainActivity : AppCompatActivity() { // 앱이 최초 실행했을 때 수
             finish()
         }
 
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId)
+        {
+            R.id.one -> Toast.makeText(applicationContext, "접근성", Toast.LENGTH_SHORT).show()
+            R.id.two -> Toast.makeText(applicationContext, "이메일", Toast.LENGTH_SHORT).show()
+            R.id.three -> Toast.makeText(applicationContext, "메시지", Toast.LENGTH_SHORT).show()
+
+        }
+        layout_drawer.closeDrawers()//펼쳐져있는데 네비게이션 drawer 메뉴를 닫아주라
+        return false
+    }
+
+    override fun onBackPressed() {
+        if(layout_drawer.isDrawerOpen(GravityCompat.START)) {
+            layout_drawer.closeDrawers()
+        }
+        else {
+            super.onBackPressed() //일단 백버튼 기능 실행
+        }
     }
 }
